6131b9f03a0f2dc486cfd2a420e322d875ad2db1
Nathan Renko <nathanrenko@danger-zone-tactical.ru>
2020-04-21 16:23:50 +0500

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Class-room';
+import ClassRoom from './Class-room';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClassRoom />
   </React.StrictMode>,
   document.getElementById('root')
 );
