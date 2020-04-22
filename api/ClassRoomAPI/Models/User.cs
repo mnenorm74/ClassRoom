e7c75cc1c8abe11e1eac316e899dfe284d1afbeb
Natalia <natasha.rogachyova@mail.ru>
2020-04-22 16:56:45 +0500

    Add User API methods and bugs fixed

--- api/ClassRoomAPI/Models/User.cs
+++ api/ClassRoomAPI/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,35 @@ namespace ClassRoomAPI.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public User(User user)
+        {
+            Id = user.Id;
+            Username = user.Username;
+            Fullname = user.Fullname;
+            Avatar = user.Avatar;
+        }
+        public User()
+        {
+
+        }
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public byte[] Avatar { get; set; } /*сделать дефолтную фотку*/
+        public void Update(User user)
+        {
+            if(user.Username != Username && user.Username != "")
+            {
+                Username = user.Username;
+            }
+            if(user.Fullname != Fullname && user.Fullname != "")
+            {
+                Fullname = user.Fullname;
+            }
+            if(user.Avatar != Avatar && user.Avatar != null)
+            {
+                Avatar = user.Avatar;
+            }
+        }
     }
 }
