38e2db010c96eafa20043a66cd4f3f4eb3c16332
Olga Bursina <bursina.olya@yandex.ru>
2020-04-25 11:54:24 +0500

    Simple newsPage realization added

--- src/pages/newsPage.js
+++ src/pages/newsPage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import NewsItem from "../components/News/newsItem";
+import News from "../components/News/newsDB";
+import '../cssDirectory/newsPage.css';
 
 function NewsPage() {
     return (
-        <div>News Page</div>
+        <div id="news">
+            <NewsItem author={News[0].author} pubDate={News[0].pubDate} article={News[0].article}/>
+            <NewsItem author={News[0].author} pubDate={News[0].pubDate} article={News[0].article}/>
+            <NewsItem author={News[0].author} pubDate={News[0].pubDate} article={News[0].article}/>
+            <NewsItem author={News[0].author} pubDate={News[0].pubDate} article={News[0].article}/>
+            <NewsItem author={News[0].author} pubDate={News[0].pubDate} article={News[0].article}/>
+        </div>
     )
 }
 
