75344a6f2b39fb317b876740ad177efba3bb3add
Olga Bursina <bursina.olya@yandex.ru>
2020-05-05 07:26:20 +0500

    Added button to news adding

--- src/pages/newsPage.js
+++ src/pages/newsPage.js
@@ -5,13 +5,16 @@ import '../cssDirectory/newsPage.css';
 
 function NewsPage() {
     return (
-        <>
+        <div id="newsContent">
+            <div id="newsHeader">
+                <button id="newsAdding"></button>
+            </div>
             <div id="news">
                 {News.map(news => (
                     <NewsItem author={news.author} pubDate={news.pubDate} article={news.article}/>
                 ))}
             </div>
-        </>
+        </div>
     )
 }
 
