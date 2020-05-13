5e166fcbc2a72e3ad9624636838e904790d4bd27
Olga Bursina <bursina.olya@yandex.ru>
2020-05-13 21:19:14 +0500

    Menu design refactored

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -12,24 +12,25 @@ function Menu() {
     let collapseButton = <button className="collapseButton" onClick={event => {
         let element = event.target;
         let menu = document.querySelector('#menuContainer');
-        menu.style.left = !isMoveOut ? '-17vw' : '0'
-        document.querySelector('#pageContainer').style.marginLeft = !isMoveOut ? '8vw' : '25vw';
-        document.querySelector('#pageContainer').style.width = !isMoveOut ? '92vw' : '75vw';
+        menu.style.left = !isMoveOut ? '-12vw' : '0'
+        document.querySelector('#pageContainer').style.marginLeft = !isMoveOut ? '8vw' : '18vw';
+        document.querySelector('#pageContainer').style.width = !isMoveOut ? '92vw' : '82vw';
         element.style.transform = !isMoveOut ? 'rotate(-180deg)' : 'rotate(0deg)';
         moveOut(!isMoveOut);
     }}/>
     return (
         <div id="menu">
             <div id="itemContainer">
-                <Profile/>
-                <MenuItem text={"Главная"} link={Page.MainPage} img='../icons/insert_drive_file-white-18dp.svg'/>
-                <MenuItem text={"Хранилище"} link={Page.StoragePage} img='../icons/insert_drive_file-white-18dp.svg'/>
-                <MenuItem text={"Новости"} link={Page.NewsPage} img='../icons/insert_drive_file-white-18dp.svg'/>
-                <MenuItem text={"Расписание"} link={Page.SchedulePage} img='../icons/insert_drive_file-white-18dp.svg'/>
-                <MenuItem text={"Список группы"} link={Page.GroupListPage} img='../icons/insert_drive_file-white-18dp.svg'/>
+                <div id="applicationLogo"></div>
+                <MenuItem text={"Главная"} link={Page.MainPage}/>
+                <MenuItem text={"Хранилище"} link={Page.StoragePage}/>
+                <MenuItem text={"Новости"} link={Page.NewsPage}/>
+                <MenuItem text={"Расписание"} link={Page.SchedulePage}/>
+                <MenuItem text={"Список группы"} link={Page.GroupListPage}/>
             </div>
             {collapseButton}
             <div id="menuOptions">
+                <Profile/>
                 {menuOption("Настройки", Page.OptionsPage)}
                 {menuOption("Выход", Page.AuthorizationPage)}
             </div>
--- src/components/menu/menuItem.js
+++ src/components/menu/menuItem.js
@@ -1,35 +1,15 @@
 import React from "react";
 import './menuItems.css'
 import ReactDOM from "react-dom";
-import Page from "../../pages/pageProvider";
-import Logo from './logoContainer'
 
-function MenuItem({link, text, img}) {
-    let icon = '';
-    switch (link) {
-        case Page.MainPage:
-        icon = Logo.mainLogo
-            break
-        case Page.NewsPage:
-            icon = Logo.newsLogo
-            break
-        case Page.SchedulePage:
-            icon = Logo.scheduleLogo
-            break
-        case Page.StoragePage:
-            icon = Logo.storageLogo
-            break
-        case Page.GroupListPage:
-            icon = Logo.groupListLogo
-            break
-    }
+function MenuItem({link, text}) {
     return (
-        <div className='navMenuItem' onClick={event => {
+        <div className='navMenuItem' onClick={() => {
             ReactDOM.render(
                 link(),
                 document.getElementById('pageContainer')
             );
-        }}><img className='ico' src={icon}/>
+        }}>
             <span className='itemName'>{text}</span>
         </div>
     )
