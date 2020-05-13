0cb47de9989cdc1ce493e0c6474adca0540ce84e
Olga Bursina <bursina.olya@yandex.ru>
2020-05-13 10:43:31 +0500

    Some commentsDB added

---
+++ src/components/News/newsCommentsDB.js
@@ -0,0 +1,9 @@
+const NewsComments = [
+    {Id: "37c1ef89-89fa-4a70-84d1-0bf985b0b0a3", Content: "вау", Date: "01.05.2020 15:45", AuthorId: "b2b9dddc-fb6c-4c1d-abb7-fb49aa90720a"},
+    {Id: "0d619606-b841-4d4a-8fd4-8f817074d0fa", Content: "очень интересно", Date: "05.05.2020 10:25", AuthorId: "9a2b6834-bf54-46b4-bc6b-ceb4be74ad8d"},
+    {Id: "f4f16550-4ac2-4915-bef9-fe3ff0ae8c14", Content: "круто", Date: "05.05.2020 11:11", AuthorId: "b2b9dddc-fb6c-4c1d-abb7-fb49aa90720a"},
+    {Id: "ed65fabc-d8e7-483c-8bdc-8cfa26589b9b", Content: "ого", Date: "10.05.2020 15:45", AuthorId: "9a2b6834-bf54-46b4-bc6b-ceb4be74ad8d"},
+    {Id: "a66e2148-284e-4490-b261-5b9bb03d9508", Content: "вот это новость", Date: "01.05.2020 16:05", AuthorId: "9a2b6834-bf54-46b4-bc6b-ceb4be74ad8d"}
+]
+
+export default NewsComments
