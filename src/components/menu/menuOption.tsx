f9f9f5ebac115cd2982469a23b70ad39dd49f29d
Nathan Renko <dima.polezhaew@ya.ru>
2020-05-14 14:56:15 +0500

    Preparing to redesign

--- src/components/menu/menuOption.tsx
+++ src/components/menu/menuOption.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function menuOption(name: string, link: ()=>JSX.Element) {
+function menuOption(name: string, link: () => JSX.Element) {
     return (
         <div className='menuOption' onClick={() => {
             ReactDOM.render(
