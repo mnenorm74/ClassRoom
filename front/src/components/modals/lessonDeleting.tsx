0ab9468727b61425b0b5efe273389b1b4fb2175f
Olga Bursina <bursina.olya@yandex.ru>
2020-05-28 20:54:37 +0500

    deleting options added

--- front/src/components/modals/lessonDeleting.tsx
+++ front/src/components/modals/lessonDeleting.tsx
@@ -23,14 +23,14 @@ function lessonDeleting(id:any, day:any) {
                         &times;
                     </a>
                     <div className="header" id="deletingHeader">Удалить запись</div>
-                    {/*<div id="deletingOptions">
+                    <div id="deletingOptions">
                         <p className="scheduleRadio"><input name="deleting" type="radio" checked/>Удалить текущую</p>
                         <p className="scheduleRadio"><input name="deleting" type="radio"/>Удалить все повторяющиеся</p>
-                    </div>*/}
-                    <p id="deletingMessage">Данная запись будет удалена из всех дней. Вы уверены, что хотите продлжить?</p>
+                    </div>
+                    {/*<p id="deletingMessage">Данная запись будет удалена из всех дней. Вы уверены, что хотите продлжить?</p>*/}
                     <div className="modalFooter" id="deletingFooter">
-                        <button className="sendingButton" id="positiveAnswer" onClick={onSubmit}>Да</button>
-                        <button className="sendingButton" id="negativeAnswer" onClick={close}>Нет</button>
+                        <button className="sendingButton" id="positiveAnswer" onClick={onSubmit}>Удалить</button>
+                        {/*<button className="sendingButton" id="negativeAnswer" onClick={close}>Нет</button>*/}
                     </div>
                 </div>
                 </form>
