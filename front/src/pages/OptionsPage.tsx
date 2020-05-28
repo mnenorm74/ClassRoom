71d3b85152301c685778a4c0cdb5ccbcf2bc445e
Olga Bursina <bursina.olya@yandex.ru>
2020-05-28 12:51:03 +0500

    fixed user settings

--- front/src/pages/OptionsPage.tsx
+++ front/src/pages/OptionsPage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "../cssDirectory/optionsPage.css"
-import CurrentUser from "../СurrentUserInfoDB";
-import groupDB from "../components/group/groupDB"
 
 function unblockField(selector: string) {
     //@ts-ignore
@@ -16,49 +14,55 @@ function unblockFields() {
     unblockField("#newPasswordRepeated");
     unblockField("#newPhoto");
     unblockField("#savingButton");
+    document.getElementById('savingButton')!.style.visibility = 'visible';
 }
 
 function OptionsPage() {
 
     return (
         <div id="optionsContent">
-            <p id="optionsHeader">Настройки профиля</p>
-            <div className="optionContainer">
-                <span className="optionHeader">E-mail</span>
-                <span className="optionHeader">Текущий e-mail</span>
-            </div>
-            <div className="optionContainer" id="emailChanging">
-                <span className="optionHeader">Новый e-mail</span>
-                <input className="optionInput" id="newEmail" disabled={true}/>
-            </div>
-            <div className="optionContainer">
-                <span className="optionHeader">Логин</span>
-                <span className="optionHeader">Текущий логин</span>
-            </div>
-            <div className="optionContainer" id="loginChanging">
-                <span className="optionHeader">Новый логин</span>
-                <input type="text" className="optionInput" id="newLogin" disabled={true}/>
-            </div>
-            <div id="passwordContainer">
-                <span className="optionHeader">Изменить пароль</span>
-                <div id="passwordFields">
-                    <input type="password" placeholder="Старый пароль" className="optionInput" id="oldPassword"
-                           disabled={true}/>
-                    <input type="password" placeholder="Новый пароль" className="optionInput" id="newPassword"
-                           disabled={true}/>
-                    <input type="password" placeholder="Новый пароль" className="optionInput" id="newPasswordRepeated"
-                           disabled={true}/>
+            <form>
+                <p id="optionsHeader">Настройки профиля</p>
+                <div className="optionContainer">
+                    <span className="optionHeader">E-mail</span>
+                    <span className="optionHeader">Текущий e-mail</span>
                 </div>
-            </div>
-            <div className="optionContainer">
-                <span className="optionHeader">Фото</span>
-                <input type="file" id="newPhoto" disabled={true}/>
-            </div>
+                <div className="optionContainer" id="emailChanging">
+                    <span className="optionHeader">Новый e-mail</span>
+                    <input className="optionInput" id="newEmail" disabled={true}/>
+                </div>
+                <div className="optionContainer">
+                    <span className="optionHeader">Логин</span>
+                    <span className="optionHeader">Текущий логин</span>
+                </div>
+                <div className="optionContainer" id="loginChanging">
+                    <span className="optionHeader">Новый логин</span>
+                    <input type="text" className="optionInput" id="newLogin" disabled={true}/>
+                </div>
+                <div id="passwordContainer">
+                    <span className="optionHeader">Изменить пароль</span>
+                    <div id="passwordFields">
+                        <input type="password" placeholder="Старый пароль" className="optionInput" id="oldPassword"
+                               disabled={true}/>
+                        <input type="password" placeholder="Новый пароль" className="optionInput" id="newPassword"
+                               disabled={true}/>
+                        <input type="password" placeholder="Новый пароль" className="optionInput"
+                               id="newPasswordRepeated"
+                               disabled={true}/>
+                    </div>
+                </div>
+                <div className="optionContainer">
+                    <span className="optionHeader">Фото</span>
+                    <input type="file" id="newPhoto" disabled={true}/>
+                </div>
+                <button id="savingButton" disabled={true}>Применить</button>
+            </form>
+
             <div className="optionContainer">
                 <button id="deletingAccountButton">Удалить аккаунт</button>
                 <button id="changingAccountButton" onClick={() => unblockFields()}>Изменить аккаунт</button>
-                <button id="savingButton" disabled={true}>Применить</button>
             </div>
+
         </div>
     )
 }
